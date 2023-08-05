f5edd8be7570dcd3ef3dcfec16371a83ce417e4b
Jacob Lang <jacoblang72@comcast.net>
2023-08-05 02:36:48 -0400

    laststylechange

--- src/App.js
+++ src/App.js
@@ -7,12 +7,40 @@ import Objective2 from "./Objective2";
 import Objective3 from "./Objective3";
 import Objective4 from "./Objective4";
 import Objective5 from "./Objective5";
+import { keyframes } from "styled-components";
+
+const kf = keyframes`
+50% {
+outline-offset : 4px;
+}
+100% {
+  outline-offset : 6px;
+}
+`
 
 const StyledDiv = styled.div`
 display : flex;
 justify-content : space-evenly;
 padding : 1rem;
 border-bottom : 2px solid gray;
+a {
+  margin : 1rem;
+  color : black;
+  text-decoration : none;
+  opacity : .8;
+  transition : .2s ease-in-out;
+  &:hover {
+    transform : scale(1.1);
+    opacity : 1;
+    transition : .2s ease-in-out;
+  }
+  &:active {
+    border-bottom : none;
+    outline : 2px solid lightseagreen;
+    animation : ${kf} .1s ease-in-out forwards;
+    border-radius : 5px;
+  }
+}
 `
 
 const style = {display : 'flex', }
